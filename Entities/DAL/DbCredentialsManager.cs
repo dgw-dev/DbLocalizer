b0a37136c30827d6c5fa4e442617ff4d83356597
daveywyliedev <daveywyliedev@hotmail.com>
2025-05-11 18:06:09 +0100

    DbCredentialsManager change

--- Entities/DAL/DbCredentialsManager.cs
+++ Entities/DAL/DbCredentialsManager.cs
@@ -10,12 +10,6 @@ namespace Entities.DAL
         private readonly Dictionary<string, DbCredentials> _dbCredentials = new Dictionary<string, DbCredentials>()
         {
             {
-                "ChorelyMVCContextConnection", new DbCredentials() { Username = "ChorelyUser", Password = "MyG0d1tsfull0fst@rs"}
-            } ,
-            {
-                "hka", new DbCredentials() { Username = "HKArt", Password = "MyG0d1tsfull0fst@rs"}
-            },
-            {
                 "LocalizationTest", new DbCredentials() { Username = "LocalizationUser", Password = "@LocalizationTest#"}
             }
         };
